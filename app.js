4b31bcbc06049aa66928209173338d04777485e4
mnr <mnroman@outlook.es>
2023-03-26 01:41:13 -0300

    Mensaje app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 app.get('/', function(request,result){
-    result.send('Hello World! - Aplicacion Node JS en Container')
+    result.send('Hola mundo! - Aplicacion Node JS <br>')
 })
 app.listen(3333,"0.0.0.0",function(){
-    console.log('hello world app is listening on port 3333')
+    console.log('Hola App esta escuando en el puerto 3333')
 })
