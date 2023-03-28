357adf261406774b9ce19663e5c51217bb48f4bb
powercloudtest <mnroman@outlook.es>
2023-03-28 03:38:39 +0000

    Edicion Mensaje

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ app.get('/', function(request,result){
     result.send('Hello World! - Aplicacion Node JS en Container')
 })
 app.listen(3333,"0.0.0.0",function(){
-    console.log('hello world app is listening on port 3333')
+    console.log('Hola Mundo App is listening on port 3333')
 })
